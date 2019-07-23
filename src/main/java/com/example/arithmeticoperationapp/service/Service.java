edba426f26020d5914c8feccf3b2f4ea76a22f23
guna144 <gunaapps14@gmail.com>
2019-07-23 11:45:12 +0400

    commit for testing1234567

--- src/main/java/com/example/arithmeticoperationapp/service/Service.java
+++ src/main/java/com/example/arithmeticoperationapp/service/Service.java
@@ -20,7 +20,7 @@ public class Service {
 	
 	@GetMapping("/test")
 	private String test() {
-		return "Testing done! <H1>1234</H1> testing updated";
+		return "Testing done! <H1>1234567</H1> testing updated";
 	}
 	
 	@GetMapping("/")
